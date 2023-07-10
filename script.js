e799ce8e26d071f751fc635481e27ebaf1163d95
Mayur Kapadi <125800989+mkkapadi12@users.noreply.github.com>
2023-07-10 11:37:47 +0530

    background

--- script.js
+++ script.js
@@ -2,26 +2,16 @@ const hr = document.getElementById("hr");
 const min = document.getElementById("min");
 const sec = document.getElementById("sec");
 const deg = 6;
-setInterval(()=>{
-let day = new Date();
-// console.log(day);
 
+setInterval(() => {
+  let day = new Date();
+  // console.log(day);
 
+  let hh = day.getHours() * 30;
+  let mm = day.getMinutes() * deg;
+  let ss = day.getSeconds() * deg;
 
-let hh = day.getHours() * 30;
-let mm = day.getMinutes() * deg;
-let ss = day.getSeconds() * deg;
-
-hr.style.transform = `rotateZ(${hh +mm/12}deg)`;
-min.style.transform = `rotateZ(${mm}deg)`;
-sec.style.transform = `rotateZ(${ss}deg)`;
-
-})
-
-
-
-
-// let btn = document.getElementById("btn");
-// console.log(btn);
-
-// btn.style.transform = "rotate(30deg)";
+  hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+  min.style.transform = `rotateZ(${mm}deg)`;
+  sec.style.transform = `rotateZ(${ss}deg)`;
+});
